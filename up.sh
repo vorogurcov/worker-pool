6b57dc3dba1ddd5b53187f1ae43229c7c554be43
Фролов Иван Максимович <frolov.im@work.kodeks.ru>
2025-12-29 13:24:58 +0300

    feat(prometheus): add replicas scraping support and create rbac.yaml for prometheus target scraping to work

--- up.sh
+++ up.sh
@@ -6,6 +6,7 @@ DB_FOLDER="./infra/db"
 WORKER_POOL_FOLDER="./infra/worker-pool"
 PROMETHEUS_FOLDER="./infra/monitoring/prometheus"
 
+
 kubectl apply -f ${DB_FOLDER}/k8s-pvc.yaml
 
 kubectl apply -f ${DB_FOLDER}/k8s-secret.yaml
@@ -17,6 +18,8 @@ kubectl apply -f ${DB_FOLDER}/k8s-service.yaml
 kubectl apply -f ${WORKER_POOL_FOLDER}/k8s-deployment.yaml
 
 kubectl apply -f ${WORKER_POOL_FOLDER}/k8s-service.yaml
+
+kubectl apply -f ${PROMETHEUS_FOLDER}/rbac.yaml
 
 kubectl apply -f ${PROMETHEUS_FOLDER}/k8s-config-map.yaml
 
